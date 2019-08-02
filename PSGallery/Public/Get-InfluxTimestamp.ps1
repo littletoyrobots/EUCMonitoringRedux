5779f9333ce27ea2c74ea674ab216c330fe2e7c8
Adam Yarborough <adam.yarborough@gmail.com>
2019-08-02 09:27:39 -0400

    Add Parameter for specific DateTime
    Useful in testing and validation.

--- PSGallery/Public/Get-InfluxTimestamp.ps1
+++ PSGallery/Public/Get-InfluxTimestamp.ps1
@@ -5,8 +5,14 @@ Returns current timestamp in nanosecond format
 .DESCRIPTION
 Long description
 
+.PARAMETER DateTime
+Optional parameter if you want to specify a datetime
+
 .EXAMPLE
-An example
+Get-InfluxTimeStamp
+
+.EXAMPLE
+Get-InfluxTimeStamp -DateTime (Get-Date)
 
 .NOTES
 General notes
@@ -14,22 +20,27 @@ General notes
 
 function Get-InfluxTimestamp {
     [CmdletBinding()]
-    Param ()
+    Param (
+        [datetime]$DateTime
+    )
 
     Begin {
-        # Write-Verbose "[$(Get-Date) BEGIN  ] $($myinvocation.mycommand)"
+
     } # Begin
 
     Process {
         Write-Verbose "[$(Get-Date) PROCESS] [$($myinvocation.mycommand)] Converting to Universal Time"
 
-        $DateTime = Get-Date
+        if ($null -eq $DateTime) {
+            $DateTime = Get-Date
+        }
+
         $utcDate = $DateTime.ToUniversalTime()
         # Convert to a Unix time as a double, noticed that it gets all the seconds down in the decimal if cast as a double.
         $unixTime = [double]((Get-Date -Date $utcDate -UFormat %s))
         # multiply seconds to move the decimal place.
         $nano = $unixTime * 1000000000
-        #cast as a int64 gets rid of the decimal and scientific notation.
+        # casting as an int64 gets rid of the decimal and scientific notation.
         Write-Verbose "[$(Get-Date) PROCESS] [$($myinvocation.mycommand)] Returning $([int64]$nano)"
         return [int64]$nano
     } # Process
